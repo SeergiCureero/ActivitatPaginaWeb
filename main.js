8873cbdf21b2db34efc29b0be82a28a1fe4ae36d
regi <sergicurero@gmail.com>
2024-11-15 07:23:14 +0100

    update 15/11/2024
    se añaden más etiquetas y se completa un poco más la estructura de la pagina. Se añaden elementos al documento .js per no funciona como se desea. En caso de que el error persista, descartar documento .js

--- main.js
+++ main.js
@@ -6,19 +6,34 @@ const translations = {
             subTitle:'Agencia de Viajes',
             titDestino1:'Italia',
             titDestino2:"Mexico",
-            titDestino3:"Alemania"
+            titDestino3:"Alemania",
+            titPorqueNuestraAgencia:"¿Porque viajar con nosotros?",
+            pPorqueNuestraAgencia:"Lorem ipsum y su polla.",
+            titAboutUs:"Sobre nosotros",
+            pAboutUs:"Somos una agencia de viajes joven y tenemos a Diego Hijano (2+2) en nuestro equipo.",
+            pFooter:"Contacto    © 2024 Reisen Club    Politica de privacidad"
     },
     ca : {  buttonSwitchLanguage:'ENG',
             subTitle:'Agencia de Viatges',
             titDestino1:'Itàlia',
             titDestino2:"Mèxic",
-            titDestino3:"Alemanya"
+            titDestino3:"Alemanya",
+            titPorqueNuestraAgencia:"Per que viatjar amb nosaltres?",
+            pPorqueNuestraAgencia:"Lorem ipsum i la seva polla.",
+            titAboutUs:"Sobre nosaltres",
+            pAboutUs:"Som una agència de viatges jove i tenim al Diego Hijano (2+2) al nostre equip.",
+            pFooter:"Contacte    © 2024 Reisen Club    Política de privacitat"
     },      
     en : {  buttonSwitchLanguage:'ESP',
             subTitle:'Travel Agency',
             titDestino1:'Italy',
             titDestino2:"Mexico",
-            titDestino3:"Germany"
+            titDestino3:"Germany",
+            titPorqueNuestraAgencia:"Why travel with us?",
+            pPorqueNuestraAgencia:"Lorem ipsum and their dick.",
+            titAboutUs:"About us",
+            pAboutUs:"We are young team, with fresh ideas and we have Diego Hijano in our team.",
+            pFooter:"Contact    © 2024 Reisen Club    Privacy Policy"
     }
 };
 
@@ -36,10 +51,27 @@ function switchLanguage() {
     
     
     //change text from elements
+
+    //Header
+    //Section Banner
     document.getElementById('buttonSwitchLanguage').innerText = translations[currentLang].buttonSwitchLanguage;
-    document.getElementById('subTitle').innerText = translations[currentLang].subTitle; 
+    document.getElementById('subTitle').innerText = translations[currentLang].subTitle;
+
+    //Main
+    //Section "Destinies"
     document.getElementById('titDestino1').innerText = translations[currentLang].titDestino1; 
     document.getElementById('titDestino2').innerText = translations[currentLang].titDestino2;
     document.getElementById('titDestino3').innerText = translations[currentLang].titDestino3;
+    //Section "Why Our Agency?""
+    document.getElementById('titPorqueNuestraAgencia').innerText = translations[currentLang].titPorqueNuestraAgencia; 
+    document.getElementById('pPorqueNuestraAgencia').innerText = translations[currentLang].pPorqueNuestraAgencia;
+    //Section "About Us"
+    document.getElementById('titAboutUs').innerText = translations[currentLang].titAboutUs; 
+    document.getElementById('pAboutUs').innerText = translations[currentLang].pAboutUs;
+
+    //Footer
+    //Section "Footer"
+    document.getElementById('pFooter').innerText = translations[currentLang].pFooter;
+
 } 
 
