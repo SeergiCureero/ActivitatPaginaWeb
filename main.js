3075d1c517dbc1e86f3189575810f35f2fe91e04
regi <sergicurero@gmail.com>
2024-11-21 12:09:00 +0100

    Update de TyC y parametrización de los precios en todos los textos donde aparecen

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 //Prizes
-prizeC1D1 = 500;    //München
-prizeC2D1 = 500;    //Berlin
-prizeC3D1 = 500;    //Köln
-prizeC1D2 = 500;    //Milano
-prizeC2D2 = 500;    //Roma
-prizeC3D2 = 500;    //Venezia
-prizeC1D3 = 500;    //
-prizeC2D3 = 500;
-prizeC3D3 = 500;
+var prizes = {
+    //usad . para separar decimales de numeros enteros
+    C1D1 : '2199.99',    //München
+    C2D1 : '1259.89',    //Berlin
+    C3D1 : '1240.99',    //Köln
+    C1D2 : '2',    //Milano
+    C2D2 : '530',    //Roma
+    C3D2 : '500',    //Venezia
+    C1D3 : '500',    //
+    C2D3 : '500',
+    C3D3 : '500'
+}
 
-
-
+//Translations
 const translations = {
     es : {  
-            botonCambioIdioma:"Idioma",
+            botonCambioIdioma:"IDIOMA",
             textSwitchLanguage_ES:"Castellano",
             textSwitchLanguage_CA:"Catalán",
             textSwitchLanguage_EN:"Inglés",
@@ -23,8 +25,13 @@ const translations = {
             navBarCountries:"Países",
             navBarAboutUs:"Sobre Nosotros",
             subTitle:"Agencia de Viajes",
+            subTitle1:"Pintor Austriaco",
+            subTitle2:"SUBTITOL ITALIA",
+            subTitle3:"SUBTITOL MEXIC",
             titDestino1:"Alemania",
+            pTitDestino1: "Viaja a Alemania desde 1000€ todo incluido",
             titDestino2:"Italia",
+            pTitDestino2: "Viaja a Italia desde 1000€ todo incluido",
             titDestino3:"Mexico",
             titPorqueNuestraAgencia:"Porque elegir nuestra agencia",
             pPorqueNuestraAgencia:"Lorem ipsum y su polla.",
@@ -35,28 +42,39 @@ const translations = {
             //TEXT FROM DESTINY PAGES
             //Destiny 1 (Germany)
             C1D1:"Munich",
-            T1C1D1:`La capital de Baviera, casa de la cerveza, y una de las ciudades con más relevancia en la historia reciente de Alemania y del mundo. Disfruta en uno de los mil Biergarten de Münich por solo ${prizeC1D1}€`,
+            T1C1D1:`La capital de Baviera, casa de la cerveza, y una de las ciudades con más relevancia en la historia reciente de Alemania y del mundo. Disfruta en uno de los mil Biergarten de Münich por solo ${prizes.C1D1}€`,
             C2D1:"Berlín",
-            T1C2D1:`No hay ciudad como Berlín. Con un viaje no te la acabas, pero con nosotros verás mucho por solo ${prizeC2D1}€`,
+            T1C2D1:`No hay ciudad como Berlín. Con un viaje no te la acabas, pero con nosotros verás mucho por solo ${prizes.C2D1}€`,
             C3D1:"Colonia",
-            T1C3D1:`¿Amante del carnaval? Colònia té tiene el mejor carnaval de toda Europa. Disfruta de un carnaval inolvidable por solo ${prizeC3D1}€`,
+            T1C3D1:`¿Amante del carnaval? Colònia té tiene el mejor carnaval de toda Europa. Disfruta de un carnaval inolvidable por solo ${prizes.C3D1}€`,
             //Destiny 2 Italy
-            C1D1:"Milán",
-            T1C1D1:`MILAN ${prizeC1D1}€`,
-            C2D1:"Roma",
-            T1C2D1:`ROMA ${prizeC2D1}€`,
-            C3D1:"Venecia",
-            T1C3D1:`VENECIA${prizeC3D1}€`,
+            C1D2:"Milán",
+            T1C1D2:`MILAN ${prizes.C1D2}€`,
+            C2D2:"Roma",
+            T1C2D2:`ROMA ${prizes.C2D2}€`,
+            C3D2:"Venecia",
+            T1C3D2:`VENECIA${prizes.C3D2}€`,
             //Destiny 3 Mexico
-            C1D1:"",
-            T1C1D1:`${prizeC1D1}€`,
-            C2D1:"",
-            T1C2D1:`${prizeC2D1}€`,
-            C3D1:"",
-            T1C3D1:`${prizeC3D1}€`
-    },
+            C1D3:"",
+            T1C1D3:`${prizes.C1D3}€`,
+            C2D3:"",
+            T1C2D3:`${prizes.C2D3}€`,
+            C3D3:"",
+            T1C3D3:`${prizes.C3D3}€`,
+
+            //TEXT FROM TERMS AND CONDITIONS (TyC)
+            t1TyC:"Terminos y condiciones generales",
+            p1TyC:"Reisen  Club (de aquí en adelante: la Agencia), como agencia de viajes de ambito internacional, pero con sede fiscal en España, se adhiere a todas las normativas de seguridad deribadas de leyes con efecto en el Reino de España y Unión Europea.",
+            p2TyC:"La Agencia ofrece a sus clientes la posibilidad de contratar un seguro de viajes de la aseguradora que el cliente desee.",
+            p3TyC:"La Agencia no hará devoluciones de dinero ni dará ningun tipo de compensación a causa de vuelos retrasados o cancelados. Si desea recibir compensación, rogamos que lea las condiciones de la aseguranza de viajes que contrate.",
+            p4TyC:"Reisen Club ofrece devoluciones en caso de cancelación del servicio contratado si éste se cancela 1 mes antes de la fecha de salida. Para pedir una devolución, deberá llamar a +35 685780630 o escribir a ",
+            p5TyC:"Si una vez en el pais de destino tiene alguna queja en relación al hotel y desea cambiarlo, no dude en contactarnos.",
+            p6TyC:"Si desea contratar un servicio, deberá pagar con tarjeta u ofreciendo a su primogénito a la Agencia para que lo podamos vender como trabajador de Temu en algun país en vias de desarrollo.",
+            p7TyC:"En caso de que, por circumstancias de fuerza mayor (entiendase por fuerza mayor un accidente que derive en hospitalización, muerte, ser del Real Madrid o tener nacionalidad Francesa), se deba cancelar el viaje, el cliente deberá abonar la cantidad total del viaje más gastos de gestión y un importe ligado a las molestias ocasionadas. De no ser así, la Agencia se verá obligada a tomar acciones legales.",
+            p8TyC:"¡Muchas gracias por contratar nuestros servicios!"
+        },
     ca : {  
-            botonCambioIdioma:"Llengua",
+            botonCambioIdioma:"LLENGUA",
             textSwitchLanguage_ES:"Castellà",
             textSwitchLanguage_CA:"Català",
             textSwitchLanguage_EN:"Anglès",
@@ -66,8 +84,13 @@ const translations = {
             navBarCountries:"Països",
             navBarAboutUs:"Sobre Nosaltres",
             subTitle:"Agència de Viatges",
+            subTitle1:"Pintor Austríac",
+            subTitle2:"SUBTITOL ITALIA",
+            subTitle3:"SUBTITOL MEXIC",
             titDestino1:"Alemanya",
+            pTitDestino1: "Viatja a Alemanya des de 1000€ tot inclòs",
             titDestino2:"Itàlia",
+            pTitDestino2: "Viatja a Itàlia des de 1000€ tot inclòs",
             titDestino3:"Mèxic",
             titPorqueNuestraAgencia:"Perque escollir la nostra agència",
             pPorqueNuestraAgencia:"Lorem ipsum i la seva polla.",
@@ -77,15 +100,27 @@ const translations = {
 
             //TEXT FROM DESTINY PAGES
             //Destiny 1 (Germany)
-            C1D1:"Munic",
-            T1C1D1:`La capital de Baviera, casa de la cervesa, i una de les ciutats amb més rellevancia a la història recent d'Alemanya i del mon. Gaudeix en un dels milers Biergarten que trobaràs a Munic per sols ${prizeC1D1}€`,
+            C1D1:"Múnic",
+            T1C1D1:`La capital de Baviera, casa de la cervesa, i una de les ciutats amb més rellevància a la història recent d'Alemanya i del món. Gaudeix en un dels milers Biergarten que trobaràs a Múnic per sols ${prizes.C1D1}€`,
             C2D1:"Berlín",
-            T1C2D1:`No hi ha ciutat com Berlín. Amb un viatge no te l'acabes, però amb nosaltres veuràs molt per sols ${prizeC2D1}€`,
+            T1C2D1:`No hi ha ciutat com Berlín. Amb un viatge no te l'acabes, però amb nosaltres veuràs molt per sols ${prizes.C2D1}€`,
             C3D1:"Colònia",
-            T1C3D1:`Amant del carnaval? Colonia té el millor carnaval de tota Europa. Gaudeix d'un carnaval inoblidable per sols ${prizeC3D1}€`,
-    },      
+            T1C3D1:`Amant del carnaval? Colònia té el millor carnaval de tota Europa. Gaudeix d'un carnaval inoblidable per sols ${prizes.C3D1}€`,
+
+            //TEXT FROM TERMS AND CONDITIONS (TyC)
+            t1TyC:"Termes i condicions generals",
+            p1TyC:"Reisen  Club (d'aquí endavant: l'Agència), com agència de viatges d'àmbit internacional, però amb seu fiscal a Espanya, s'adhereix a totes les normatives de seguretat derivades de lleis amb efecte al Regne d'Espanya i Unió Europea.",
+            p2TyC:"L'Agència ofereix als seus clients la possibilitat de contractar una assegurança de viatges de l'asseguradora que el client desitgi.",
+            p3TyC:"L'Agència no farà devolucions de diners ni donarà cap mena de compensació a causa de vols endarrerits o cancel·lats. Si desitja rebre compensació, preguem que llegeixi les condicions de l'assegurança de viatges que contracti.",
+            p4TyC:"Reisen Club ofereix devolucions en cas de cancel·lació del servei contractat si aquest es cancel·la 1 mes abans de la data de sortida. Per demanar una devolució haurà de trucar al +35 685780630 o escriure a ",
+            p5TyC:"Si una vegada al país de destí té alguna queixa en relació amb l'hotel i desitja canviar-ho, no dubti a contactar amb nosaltres.",
+            p6TyC:"Si desitja contractar un servei, haurà de pagar amb targeta o oferint al seu primogènit a l'Agència perquè el puguem vendre com a treballador de Temu a algun país en vies de desenvolupament.",
+            p7TyC:"En cas que, per circumstàncies de força major (entengui's per força major un accident que derivi en hospitalització, mort, ser del Real Madrid o tenir nacionalitat Francesa), s'hagi de cancel·lar el viatge, el client haurà d'abonar la quantitat total del viatge més despeses de gestió i un import lligat a les molesties ocasionades. En cas de no ser així, l'Agència es veurà obligada a prendre accions legals.",
+            p8TyC:"Moltes gràcies per contractar els nostres serveis!"
+    
+        },      
     en : {  
-            botonCambioIdioma:"Language",
+            botonCambioIdioma:"LANGUAGE",
             textSwitchLanguage_ES:"Spanish",
             textSwitchLanguage_CA:"Catalan",
             textSwitchLanguage_EN:"English",
@@ -107,14 +142,14 @@ const translations = {
             //TEXT FROM DESTINY PAGES
             //Destiny 1 (Germany)
             C1D1:"Munich",
-            T1C1D1:`The capital of Bavaria, home of the best beers in the world and one of the cities with the most relevance in the recent history of Germany and the world. enjoy visiting one of its many Biergarten for only ${prizeC1D1}€`,
+            T1C1D1:`The capital of Bavaria, home of the best beers in the world and one of the cities with the most relevance in the recent history of Germany and the world. enjoy visiting one of its many Biergarten for only ${prizes.C1D1}€`,
             C2D1:"Berlin",
-            T1C2D1:`There's no city like Berlin. You won't see it all in just one trip, but travelling with us is a good headstart, and it only costs ${prizeC2D1}€`,
+            T1C2D1:`There's no city like Berlin. You won't see it all in just one trip, but travelling with us is a good headstart, and it only costs ${prizes.C2D1}€`,
             C3D1:"Cologne",
-            T1C3D1:`Carnival lover? Cologne has THE BEST carnvial in Europe. Enjoy an unforgettable carnival for only ${prizeC3D1}€`,
-    },
+            T1C3D1:`Carnival lover? Cologne has THE BEST carnvial in Europe. Enjoy an unforgettable carnival for only ${prizes.C3D1}€`,
+        },
     it : {  
-            botonCambioIdioma:"Lingua",
+            botonCambioIdioma:"LINGUA",
             textSwitchLanguage_ES:"Spagnolo",
             textSwitchLanguage_CA:"Catalano",
             textSwitchLanguage_EN:"Inglese",
@@ -136,12 +171,12 @@ const translations = {
             //TEXT FROM DESTINY PAGES
             //Destiny 1 (Germany)
             C1D1:"Monaco",
-            T1C1D1:`La capital di Baviera, casa della birra, e una delle città piu rilevanti dell'istoria ricente dalla Germania e del mondo. Goditi a qualcuno dei mille Biergarten che troverai a Monaco per solo ${prizeC1D1}€`,
+            T1C1D1:`La capital di Baviera, casa della birra, e una delle città piu rilevanti dell'istoria ricente dalla Germania e del mondo. Goditi a qualcuno dei mille Biergarten che troverai a Monaco per solo ${prizes.C1D1}€`,
             C2D1:"Berlino",
-            T1C2D1:`Non c'è nessuna città come Berlino. Con questo viaggio no la finisci, ma con noi vedrai molto per solo ${prizeC2D1}€`,
+            T1C2D1:`Non c'è nessuna città come Berlino. Con questo viaggio no la finisci, ma con noi vedrai molto per solo ${prizes.C2D1}€`,
             C3D1:"Colonia",
-            T1C3D1:`Amante del carnevale? Colonia ha il meglio carnevale d'Europa. Goditi d'un carnavale che non potrai dimenticare per solo ${prizeC3D1}€`,
-    }
+            T1C3D1:`Amante del carnevale? Colonia ha il meglio carnevale d'Europa. Goditi d'un carnavale che non potrai dimenticare per solo ${prizes.C3D1}€`,
+        }
 };
 
 
@@ -157,8 +192,7 @@ function updateLanguage() {
     const currentLang = localStorage.getItem('language');
     console.log(currentLang);   //Debug in console
 
-    console.log(document.body.id); //Debug the name (id) of the page that is being loaded
-
+    console.log(document.body.id); //Debug the name (id) of the page that is being loaded 
 
     //__Change text from elements__
     //SHARED ELEMENTS
@@ -180,6 +214,10 @@ function updateLanguage() {
 
     switch (document.body.id) { //check what page we're on
         case "indexBody":
+            
+            //Update Prizes from main page
+            updatePrizes();
+            
             //Header
             //Section Banner
             document.getElementById('subTitle').innerText = translations[currentLang].subTitle;
@@ -197,6 +235,10 @@ function updateLanguage() {
             break;
     
         case "destiny1Body":
+            document.getElementById('Name1').innerText = translations[currentLang].titDestino1;
+            document.getElementById('subTitle1').innerText = translations[currentLang].subTitle1;
+            document.getElementById('titDestino1').innerText = translations[currentLang].titDestino1;
+            document.getElementById('pTitDestino1').innerText = translations[currentLang].pTitDestino1;
             document.getElementById('C1D1').innerText = translations[currentLang].C1D1;
             document.getElementById('T1C1D1').innerText = translations[currentLang].T1C1D1;
             document.getElementById('C2D1').innerText = translations[currentLang].C2D1;
@@ -204,7 +246,12 @@ function updateLanguage() {
             document.getElementById('C3D1').innerText = translations[currentLang].C3D1;
             document.getElementById('T1C3D1').innerText = translations[currentLang].T1C3D1;
             break;
+
         case "destiny2Body":
+            document.getElementById('Name2').innerText = translations[currentLang].titDestino2;
+            document.getElementById('subTitle2').innerText = translations[currentLang].subTitle2;
+            document.getElementById('titDestino2').innerText = translations[currentLang].titDestino2;
+            document.getElementById('pTitDestino2').innerText = translations[currentLang].pTitDestino2;
             document.getElementById('C1D2').innerText = translations[currentLang].C1D2;
             document.getElementById('T1C1D2').innerText = translations[currentLang].T1C1D2;
             document.getElementById('C2D2').innerText = translations[currentLang].C2D2;
@@ -224,11 +271,36 @@ function updateLanguage() {
             
             break;
         case "TyCBody":
-            
+            document.getElementById('t1TyC').innerText = translations[currentLang].t1TyC;
+            document.getElementById('p1TyC').innerText = translations[currentLang].p1TyC;
+            document.getElementById('p2TyC').innerText = translations[currentLang].p2TyC;
+            document.getElementById('p3TyC').innerText = translations[currentLang].p3TyC;
+            document.getElementById('p4TyC').childNodes[0].innerText = translations[currentLang].p4TyC;
+            document.getElementById('p5TyC').innerText = translations[currentLang].p5TyC;
+            document.getElementById('p6TyC').innerText = translations[currentLang].p6TyC;
+            document.getElementById('p7TyC').innerText = translations[currentLang].p7TyC;
+            document.getElementById('p8TyC').innerText = translations[currentLang].p8TyC;
+
             break;
 
         default:
             break;
     }
 } 
+
+
+function updatePrizes() {
+    //Update Prizes from main page
+    prizesD1 = [parseFloat(prizes.C1D1),parseFloat(prizes.C2D1),parseFloat(prizes.C3D1)];
+    prizesD1.sort(function(a, b){return a - b});
+    console.log(prizesD1);
+    console.log(typeof(prizesD1[0]))
+    document.getElementById('mainOfferD1').innerText = (`${prizesD1[0]}€`);
+    prizesD2 = [parseFloat(prizes.C1D2),parseFloat(prizes.C2D2),parseFloat(prizes.C3D2)];
+    prizesD2.sort(function(a, b){return a - b});
+    document.getElementById('mainOfferD2').innerText = (`${prizesD2[0]}€`);
+    prizesD3 = [parseFloat(prizes.C1D3),parseFloat(prizes.C2D3),parseFloat(prizes.C3D3)];
+    prizesD3.sort(function(a, b){return a - b});
+    document.getElementById('mainOfferD3').innerText = (`${prizesD3[0]}€`);
+}
 
