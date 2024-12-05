1bb01225a74bbc86a2448c1a12bdda827ff7fd57
regi <sergicurero@gmail.com>
2024-12-05 10:10:29 +0100

    idk

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const prizes = {
     C2D2 : 567,       
     C3D2 : 30,      
     C1D3 : 60,       
-    C2D3 : 3,
-    C3D3 : 2,
+    C2D3 : 3840,
+    C3D3 : 184,
     OtherOffers1 : 30,
     OtherOffers2 : 30,
     OtherOffers3 : 30
@@ -20,14 +20,14 @@ updatePrizes();
 const translationsWithPrizes = {
     es:{
         pTitDestino1: `Viaja a Alemania desde ${lowestPrizes[0]}€ todo incluido`,
-        T1C1D1:`La capital de Baviera, casa de la cerveza, y una de las ciudades con más relevancia en la historia reciente de Alemania y del mundo. Disfruta en uno de los mil Biergarten de Münich por solo ${parseFloat(prizes.C1D1)}€`,
-        T1C2D1:`No hay ciudad como Berlín. Con un viaje no te la acabas, pero con nosotros verás mucho por solo ${parseFloat(prizes.C2D1)}€`,
-        T1C3D1:`¿Amante del carnaval? Colonia tiene el mejor carnaval de toda Europa. Disfruta de un carnaval inolvidable por solo ${parseFloat(prizes.C3D1)}€`,
+        T2C1D1:`Disfruta en uno de los mil Biergarten de Münich por solo ${(prizes.C1D1)}€`,
+        T2C2D1:`Viaja a Berlín por solo ${(prizes.C2D1)}`,
+        T2C3D1:`Disfruta de un carnaval inolvidable por solo ${(prizes.C3D1)}€`,
     
         pTitDestino2: `Viaja a Italia desde ${lowestPrizes[1]}€ todo incluido`,
-        T1C1D2:`Dicen que todos los caminos llevan a Roma. La ciudad capital del que fue el Imperio que bañó el Mediterraneo por siglos, el lugar de nacimiento de la cultura occidental, de las lenguas romances. Toda esta influencia se siente y se ve en la capital de Italia. Pasea por donde caminaban los emperadores romanos por solo ${prizes.C1D2}€`,
-        T1C2D2:`La segunda ciudad más grande de Italia. Una ciudad con una rica historia y preciosa arquitectura. Tanto si buscas una ciudad medieval como una urbe moderna y europea, ¡Milán es tu ciudad! Visitala por solo ${prizes.C2D2}€`,
-        T1C3D2:`FLORENCIA ${prizes.C3D2}€`,
+        T2C1D2:`Pasea por donde caminaban los emperadores romanos por solo ${prizes.C1D2}€`,
+        T2C2D2:`Tanto si buscas una ciudad medieval como una urbe moderna y europea, ¡Milán es tu ciudad! Visitala por solo ${prizes.C2D2}€`,
+        T2C3D2:`FLORENCIA ${prizes.C3D2}€`,
     
         pTitDestino3: `Viaja a México con todo incluido desde ${lowestPrizes[2]}€.`,
         T2C1D3:`Descubre Ciudad de México desde ${prizes.C1D3}€ todo incluido.`,
@@ -40,14 +40,14 @@ const translationsWithPrizes = {
     },
     ca:{
         pTitDestino1: `Viatja a Alemanya des de ${lowestPrizes[0]}€ tot inclòs`,
-        T1C1D1:`La capital de Baviera, casa de la cervesa, i una de les ciutats amb més rellevància a l'història recent d'Alemanya i del mon. Gaudeix a un dels milers Biergarten de Múnic per sols ${prizes.C1D1}€`,
-        T1C2D1:`No hi ha ciutat com Berlin. Amb un viatje no te l'acabes, però amb nosaltres veuras molt per sols ${prizes.C2D1}€`,
-        T1C3D1:`Amant del carnaval? Colònia té el millor carnaval de tota Europa. Gaudeix d'un carnaval inoblidable per sols ${prizes.C3D1}€`,
+        T2C1D1:`Gaudeix a un dels milers Biergarten de Múnic per sols ${prizes.C1D1}€`,
+        T2C2D1:`Viatja a Berlín per sols 500€${prizes.C2D1}€`,
+        T2C3D1:`Gaudeix d'un carnaval inoblidable per sols ${prizes.C3D1}€`,
     
         pTitDestino2: `Viatja a Itàlia des de ${lowestPrizes[1]}€ tot inclòs`,
-        T1C1D2:`Diuen que tots els camins porten a Roma. La ciutat del que va ser l'Imperi que banyà el Mediterrani per segles, el lloc on va naixer la cultura occidental, les llengües romaniques. Tota aquesta influencia se sent i es veu a la que és avui la capital d'Itàlia. Passeja  per on caminaven emperadors romans per sols ${prizes.C1D2}€`,
-        T1C2D2:`La segona ciutat més gran d'Itàlia. Una ciutat amb una rica història i preciosa arquitectura. Tant si busques una ciutat medieval o una metropoli moderna i europea, Milà és la teva ciutat! Visita-la per sols ${prizes.C2D2}€`,
-        T1C3D2:`FLORENCIA ${prizes.C3D2}€`,
+        T2C1D2:`Passeja  per on caminaven emperadors romans per sols ${prizes.C1D2}€`,
+        T2C2D2:`Tant si busques una ciutat medieval o una metropoli moderna i europea, Milà és la teva ciutat! Visita-la per sols ${prizes.C2D2}€`,
+        T2C3D2:`FLORENCIA ${prizes.C3D2}€`,
     
         pTitDestino3: `Viatja a Mèxic amb tot inclos des de ${lowestPrizes[2]}€.`,
         T2C1D3:`Descobreix Ciudad de México des de ${prizes.C1D3}€ tot inclos.`,
@@ -175,36 +175,45 @@ function updateLanguage(data) {
         case "destiny1Body":
             //Update Prizes from
             updatePrizes();
+            document.getElementsByTagName('title').innerText = data[currentLang].pageNameD1;
             document.getElementById('Name1').innerText = data[currentLang].titDestino1;
             document.getElementById('subTitle1').innerText = data[currentLang].subTitle1;
             document.getElementById('titDestino1').innerText = data[currentLang].titDestino1;
             document.getElementById('pTitDestino1').innerText = translationsWithPrizes[currentLang].pTitDestino1;
             document.getElementById('C1D1').innerText = data[currentLang].C1D1;
-            document.getElementById('T1C1D1').innerText = translationsWithPrizes[currentLang].T1C1D1;
+            document.getElementById('T1C1D1').innerText = data[currentLang].T1C1D1;
+            document.getElementById('T2C1D1').innerText = translationsWithPrizes[currentLang].T2C1D1;
             document.getElementById('C2D1').innerText = data[currentLang].C2D1;
-            document.getElementById('T1C2D1').innerText = translationsWithPrizes[currentLang].T1C2D1;
+            document.getElementById('T1C2D1').innerText = data[currentLang].T1C2D1;
+            document.getElementById('T2C2D1').innerText = translationsWithPrizes[currentLang].T2C2D1;
             document.getElementById('C3D1').innerText = data[currentLang].C3D1;
-            document.getElementById('T1C3D1').innerText = translationsWithPrizes[currentLang].T1C3D1;
+            document.getElementById('T1C3D1').innerText = data[currentLang].T1C3D1;
+            document.getElementById('T2C3D1').innerText = translationsWithPrizes[currentLang].T2C3D1;
             break;
 
         case "destiny2Body":
             //Update Prizes 
             updatePrizes();
+            document.getElementsByTagName('title').innerText = data[currentLang].pageNameD2;
             document.getElementById('Name2').innerText = data[currentLang].titDestino2;
             document.getElementById('subTitle2').innerText = data[currentLang].subTitle2;
             document.getElementById('titDestino2').innerText = data[currentLang].titDestino2;
             document.getElementById('pTitDestino2').innerText = translationsWithPrizes[currentLang].pTitDestino2;
             document.getElementById('C1D2').innerText = data[currentLang].C1D2;
-            document.getElementById('T1C1D2').innerText = translationsWithPrizes[currentLang].T1C1D2;
+            document.getElementById('T1C1D2').innerText = data[currentLang].T1C1D2;
+            document.getElementById('T2C1D2').innerText = translationsWithPrizes[currentLang].T2C1D2;
             document.getElementById('C2D2').innerText = data[currentLang].C2D2;
-            document.getElementById('T1C2D2').innerText = translationsWithPrizes[currentLang].T1C2D2;
+            document.getElementById('T1C2D2').innerText = data[currentLang].T1C2D2;
+            document.getElementById('T2C2D2').innerText = translationsWithPrizes[currentLang].T2C2D2;
             document.getElementById('C3D2').innerText = data[currentLang].C3D2;
-            document.getElementById('T1C3D2').innerText = translationsWithPrizes[currentLang].T1C3D2;
+            document.getElementById('T1C3D2').innerText = data[currentLang].T1C3D2;
+            document.getElementById('T2C3D2').innerText = translationsWithPrizes[currentLang].T2C3D2;
             break;
     
         case "destiny3Body":
             //Update Prizes from main page
             updatePrizes();
+            document.getElementsByTagName('title').innerText = data[currentLang].pageNameD3;
             document.getElementById('Name3').innerText = data[currentLang].titDestino3;
             //document.getElementById('titDestino3').innerText = data[currentLang].titDestino3;
             //document.getElementById('pTitDestino3').innerText = translationsWithPrizes[currentLang].pTitDestino3;
@@ -223,6 +232,7 @@ function updateLanguage(data) {
             
             break;
         case "countriesBody":
+            document.getElementsByTagName('title').innerText = data[currentLang].pageNameCOUNTRIES;
             document.getElementById('ourHighlightedDestinies').innerText = data[currentLang].ourHighlightedDestinies;
             document.getElementById('highlightedDestiny1').innerText = data[currentLang].highlightedDestiny1;
             document.getElementById('highlightedDestiny2').innerText = data[currentLang].highlightedDestiny2;
@@ -234,12 +244,17 @@ function updateLanguage(data) {
             document.getElementById('H1COUNTRIES').innerText = data[currentLang].H1COUNTRIES;
             document.getElementById('H2COUNTRIES').innerText = data[currentLang].H2COUNTRIES;
             document.getElementById('H3COUNTRIES').innerText = data[currentLang].H3COUNTRIES;
+            document.getElementById('T1H1COUNTRIES').innerText = data[currentLang].T1H1COUNTRIES;
+            document.getElementById('T1H2COUNTRIES').innerText = data[currentLang].T1H2COUNTRIES;
+            document.getElementById('T1H3COUNTRIES').innerText = data[currentLang].T1H3COUNTRIES;
             
             break;
         case "aboutUsBody":
+            document.getElementsByTagName('title').innerText = data[currentLang].pageNameAU;
             
             break;
         case "TyCBody":
+            document.getElementsByTagName('title').innerText = data[currentLang].pageNameTyC;
             document.getElementById('t1TyC').innerText = data[currentLang].t1TyC;
             document.getElementById('p1TyC').innerText = data[currentLang].p1TyC;
             document.getElementById('p2TyC').innerText = data[currentLang].p2TyC;
